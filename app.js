ca69c742db6e488b21ab4f204344d268a285ea38
satyam <120718713+Gicharki@users.noreply.github.com>
2023-02-18 04:38:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const apiKey = "48716ee51db928e8693f12b2a85f4f97"; // Replace with your OpenWeatherMap API key
+
+		function getWeather() {
+			const cityInput = document.getElementById("city-input");
+			const city = cityInput.value;
+
+			if (city.trim() === "") {
+				alert("Please enter a city name.");
+				return;
+			}
+
+			const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+			fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					const weatherCards = document.getElementById("weather-cards");
+					weatherCards.innerHTML = ""; // Clear previous cards
+
+					for (let i = 0; i < data.list.length; i += 8) {
+						const date = new Date(data.list[i].dt * 1000);
+						const iconUrl = `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`;
+						const temp = Math.round(data.list[i].main.temp);
+						const description = data.list[i].weather[0].description;
+
+						const card = `
+							<div class="card">
+								<h3>${date.toDateString()}</h3>
+								<img src="${iconUrl}" alt="${description}">
+								<p>${temp} &deg;C</p>
+                                <p>${description}</p>
+							</div>
+						`;
+						weatherCards.innerHTML += card;
+					}
+				})
+				.catch(error => {
+					console.error(error);
+					alert("Failed to get weather information. Please try again later.");
+				});
+		}
